8a827b383e3313f80b537da58e0da7cfd6ca19bd
Rahul Tarak <rahultarak12345@gmail.com>
2020-08-27 12:47:40 +0530

    💭 Text on Bubbles

--- src/components/home/TextBubble.tsx
+++ src/components/home/TextBubble.tsx
@@ -6,7 +6,7 @@ type TextBubbleProps = RecursiveNonNullable<
     IndexPageTemplateQuery
 >['markdownRemark']['frontmatter']['advice'];
 
-const TextBubble = ({ left, right }: TextBubbleProps) => {
+const TextBubble = ({ left, right, text }: TextBubbleProps) => {
     const leftImageInfo = {
         image: left,
         childImageSharp: left.childImageSharp,
@@ -23,29 +23,58 @@ const TextBubble = ({ left, right }: TextBubbleProps) => {
 
     return (
         <>
-            <div className="columns">
-                <div className="column is-4 is-offset-7 has-text-centered">
-                    <div
-                        style={{
-                            width: '100%',
-                            display: 'inline-block'
-                        }}
-                    >
-                        <PreviewCompatibleImage imageInfo={rightImageInfo} />
+            <div>
+                <div className="columns bubble-wrapper">
+                    <div className="column is-4 is-offset-7 has-text-centered bubble-image">
+                        <div
+                            style={{
+                                width: '100%',
+                                display: 'inline-block'
+                            }}
+                        >
+                            <PreviewCompatibleImage imageInfo={rightImageInfo} />
+                        </div>
+                    </div>
+                    <div className="column is-4 is-offset-7 bubble-text">
+                        <h3>{text ? text[0] : ''}</h3>
                     </div>
                 </div>
-            </div>
-            <div className="columns">
-                <div className="column is-4 is-offset-1 has-text-centered">
-                    <div
-                        style={{
-                            width: '100%',
-                            display: 'inline-block'
-                        }}
-                    >
-                        <PreviewCompatibleImage imageInfo={leftImageInfo} />
+                <div className="columns bubble-wrapper">
+                    <div className="column is-4 is-offset-1 has-text-centered bubble-image">
+                        <div
+                            style={{
+                                width: '100%',
+                                display: 'inline-block'
+                            }}
+                        >
+                            <PreviewCompatibleImage imageInfo={leftImageInfo} />
+                        </div>
+                    </div>
+                    <div className="column is-4 is-offset-1  bubble-text">
+                        <h3>{text ? text[1] : ''}</h3>
                     </div>
                 </div>
+                <style jsx>{`
+                    .bubble-text {
+                        z-index: 1;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        text-align: center;
+                    }
+                    .bubble-image {
+                        z-index: 0;
+                    }
+                    .bubble-wrapper {
+                        display: grid;
+                        grid-template: 1fr / 1fr;
+                    }
+
+                    .bubble-wrapper > * {
+                        grid-column: 1 / 1;
+                        grid-row: 1 / 1;
+                    }
+                `}</style>
             </div>
         </>
     );
